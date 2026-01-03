fbbd16537a4b1a0a8c0ccddc9b78012b8715bcc0
Mehdi <mehdichebbi111@gmail.com>
2026-01-03 17:03:46 +0100

    miss

---
+++ frontend/react/src/components/ImageComparisonSlider.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react'
+
+const ImageComparisonSlider = () => {
+  return (
+    <iframe
+      src="/comparison-slider.html"
+      title="Image Comparison Slider"
+      className="w-full border-0"
+      style={{ height: '500px' }}
+    />
+  )
+}
+
+export default ImageComparisonSlider
