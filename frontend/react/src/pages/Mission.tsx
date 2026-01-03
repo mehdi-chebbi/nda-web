8a845c03d9be3efc75d8f295db1d2506dd2f13f7
Mehdi <mehdichebbi111@gmail.com>
2026-01-03 17:11:24 +0100

    misss

--- frontend/react/src/pages/Mission.tsx
+++ frontend/react/src/pages/Mission.tsx
@@ -1,12 +1,35 @@
-import Card from '../components/Card'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 const Mission = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      if (event.data.type === 'navigate' && event.data.path) {
+        navigate(event.data.path)
+      }
+    }
+
+    window.addEventListener('message', handleMessage)
+    return () => {
+      window.removeEventListener('message', handleMessage)
+    }
+  }, [navigate])
+
   return (
-    <iframe
-      src="public\mission.html"
-      title="Image Comparison Slider"
-      className="w-full border-0"
-      style={{ height: '500px' }}
-    />
+    <div className="w-full h-screen">
+      <iframe
+        src="/mission.html"
+        title="mission"
+        className="w-full h-full border-0"
+        style={{
+          width: '100%',
+          height: '100vh',
+          border: 'none',
+        }}
+      />
+    </div>
   )
 }
 export default Mission
